7a36bff4e7aee52f54792f3514c5a67aaa39fd51
Richard Palethorpe <rpalethorpe@suse.com>
2018-10-02 12:26:28 +0200

    Add IJulia notebook

---
+++ src/notebook.jl
@@ -0,0 +1,16 @@
+import Pkg
+
+# We need to add IJulia to the shared/home project, otherwise Jupyter might
+# not be able to start it.
+Pkg.activate()
+Pkg.add("IJulia")
+
+using IJulia
+
+curdir = dirname(@__FILE__)
+
+if curdir != ""
+    notebook(dir = curdir, detached = true)
+else
+    notebook(detached = true)
+end
