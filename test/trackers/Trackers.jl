3e2f2f6081a2f28664da84f85f4ac6573d8c570e
Richard Palethorpe <rpalethorpe@suse.com>
2018-12-12 14:14:41 +0100

    Add Trackers & OpenQA tests

---
+++ test/trackers/Trackers.jl
@@ -0,0 +1,13 @@
+const SS = Trackers.StaticSession
+
+@testset "Trackers" begin
+    conf = TOML.parsefile(joinpath(@__DIR__, "../conf/trackers.toml"))
+    repo = load_trackers(conf)
+    api = repo.apis["Foos"]
+    @test api == Api{SS}("Foos", template"/bar/show_bug.cgi?id={id}")
+    @test get_tracker(repo, "foo") ==
+        Tracker{SS}(api, nothing, "foo", "wss", "foo.suse.com")
+
+    repo = load_trackers()
+    @test length(repo.instances) > 0 && length(repo.apis) > 0
+end
