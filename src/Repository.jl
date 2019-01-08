228a8eab1a5a55ed07b29d6464971f1a10fc8e2c
Richard Palethorpe <rpalethorpe@suse.com>
2019-01-08 11:28:59 +0100

    Redis load and store functions

--- src/Repository.jl
+++ src/Repository.jl
@@ -1,7 +1,35 @@
 module Repository
 
+using BSON
+using Redis
+
 "Some kind of item tracked by a tracker"
 abstract type AbstractItem end
+
+rconn = nothing
+
+function store(key::String, value::I)::Bool where {I <: AbstractItem}
+    global rconn
+
+    if rconn == nothing
+        rconn = RedisConnection()
+    end
+
+    buf = IOBuffer()
+    bson(buf, Dict(I.name.name => value))
+    set(rconn, key, String(take!(buf)))
+end
+
+function load(key::String, ::Type{I})::I where {I <: AbstractItem}
+    global rconn
+
+    if rconn == nothing
+        rconn = RedisConnection()
+    end
+
+    res = get(rconn, key)
+    BSON.load(IOBuffer(res))[I.name.name]
+end
 
 function fetch(item::I,
                in_container::C,
