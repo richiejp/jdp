3ed76e68fd09af5da17386cf67315702bb7b548c
Richard Palethorpe <rpalethorpe@suse.com>
2019-01-24 11:18:23 +0100

    Fix password arg

--- src/Repository.jl
+++ src/Repository.jl
@@ -20,7 +20,7 @@ function getconn()::RedisConnection
     if rconn == nothing
         dconf = Conf.get_conf(:data)
         rconn = RedisConnection(;host=get(dconf, "host", "127.0.0.1"),
-                                auth=get(dconf, "auth", ""))
+                                password=get(dconf, "auth", ""))
     end
 
     rconn
