20fc55805ff1cea70fb9dab6626b9ab2408ca70d
Richard Palethorpe <rpalethorpe@suse.com>
2019-02-04 10:58:13 +0100

    Add array arg test

--- test/IOHelpers.jl
+++ test/IOHelpers.jl
@@ -1,10 +1,13 @@
 @testset "Shell args parsing" begin
     defs = IOHelpers.ShellArgDefs(Set(["baz"]),
-                                  Set(["foo"]))
-    argsv = split("--foo bar --baz foo bar", " ")
+                                  Dict("foo" => String,
+                                       "wibble" => Vector{String}))
+    argsv = split("--foo bar --baz foo bar --wibble wobble,frobble",
+                  " ")
 
     args = IOHelpers.parse_args(defs, argsv)
     @test args.positional == ["foo", "bar"]
     @test args.named["baz"] == true
     @test args.named["foo"] == "bar"
+    @test args.named["wibble"] == ["wobble","frobble"]
 end
