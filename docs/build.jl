845dcbbeb8fe4a6533587c8d76b1459c5e2a65db
Richard Palethorpe <rpalethorpe@suse.com>
2019-02-05 09:32:20 +0100

    Add docs clean flag and use Documenter as normal dep

--- docs/build.jl
+++ docs/build.jl
@@ -1,20 +1,20 @@
 using Pkg
 
-Pkg.activate()
-Pkg.add("Documenter")
-
 include("../src/init.jl")
 
-# This is intentionally not included as a project dependency
 using Documenter
 
 using JDP
+import JDP.IOHelpers: ShellArgDefs, parse_args
 import JDP.BugRefsParser
+
+args = parse_args(ShellArgDefs(Set(["clean"]), Dict()), ARGS).named
 
 makedocs(
     modules = [JDP],
     format = :html,
     sitename = "JDP",
+    clean = args["clean"],
     pages = [
         "Home" => "index.md",
         "Reference" => [
