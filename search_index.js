2a6ce2c0912baaa6d47cb4904f557ae8e9118345
Richard Palethorpe <rpalethorpe@suse.com>
2019-02-21 15:16:10 +0100

    Update generated docs

--- search_index.js
+++ search_index.js
@@ -13,7 +13,71 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "JDP",
     "category": "section",
-    "text": "This is the library documentation. Installation instructions are in the README."
+    "text": "Extensible, sometimes automated, test/bug review and reporting development environment. The broader aim is to make prototyping arbitrary reporting and inter-tool workflows cheap so that experimentation in this area has a convex payoff.Initially targeted at SUSE\'s QA Kernel & Networking team\'s requirements, but this is intended to have general applicability at least within SUSE QA."
+},
+
+{
+    "location": "#Install-1",
+    "page": "Home",
+    "title": "Install",
+    "category": "section",
+    "text": "The goal is to do this in a single command, but for now it takes a few more.note: Note\nSUSE employees and associates should view this at: gitlab.suse.de/rpalethorpe/jdp"
+},
+
+{
+    "location": "#Docker-1",
+    "page": "Home",
+    "title": "Docker",
+    "category": "section",
+    "text": "You can install using Docker by doing the following from the directory where you cloned this repo. This is probably the easiest way if you just want to quickly try it out.docker build -t jdp:latest -f install/Dockerfile .Or you can substitute the build command for the following which will get a pre-built image from hub.docker.com (it may not be up to date).docker pull suserichiejp/jdp:latestThen you can inject the access details for the data cache server if you have them. Using the data cache can save a lot of time.docker build -t jdp:latest -f install/Dockerfile-slave \\\n             --build-arg REDIS_MASTER_HOST=ip-or-name \\\n             --build-arg REDIS_MASTER_AUTH=password .note: Note\nIf you pulled from dockerhub (or wherever) then you will need to change the tag name to suserichiejp/jdp:latest (or whatever).Then run itdocker run -it -p 8889:8889 jdp:latestWith a bit of luck you will see a message from Jupyter describing what to do next. The Docker image also contains two volumes which you may mount. See the Dockerfile for more info.You can use the Docker image for developing JDP itself by mounting the src volume. However this is probably not a good long term solution."
+},
+
+{
+    "location": "#Other-1",
+    "page": "Home",
+    "title": "Other",
+    "category": "section",
+    "text": "You can use install/Dockerfile as a guide. Also check conf/*.toml.You can run JDP directly from the git checkout. Just install the deps listed in the Dockerfile and modify the conf files (which should include there own documentation)."
+},
+
+{
+    "location": "#Usage-1",
+    "page": "Home",
+    "title": "Usage",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "#With-Jupyter-1",
+    "page": "Home",
+    "title": "With Jupyter",
+    "category": "section",
+    "text": "If you are using the Docker image then just browse to localhost:8889. If not then start Jupyter yourself.Open either the notebooks/Report-DataFrames.ipynb or notebooks/Propagate Bug Tags.ipynb Jupyter notebooks which are (hopefully) self documenting. I have only tested them with Jupyter itself, but there are fancier alternatives such as JupyterLab and, of course, Emacs."
+},
+
+{
+    "location": "#Other-2",
+    "page": "Home",
+    "title": "Other",
+    "category": "section",
+    "text": "You can also use the library from a Julia REPL or another project. For example in a julia REPL you could runinclude(\"src/init.jl\")Also the run directory contains scripts which are intended to automate various tasks. These can be executed with Julia in a similar way to julia run/all.jl."
+},
+
+{
+    "location": "#Automation-1",
+    "page": "Home",
+    "title": "Automation",
+    "category": "section",
+    "text": "JDP is automated using SUSE\'s internal Gitlab CI instance. Which automates building and testing the containers as well as deployment and the execution of various scripts/services. See install/gitlab-ci.*."
+},
+
+{
+    "location": "#Documentation-1",
+    "page": "Home",
+    "title": "Documentation",
+    "category": "section",
+    "text": "Further documentation can be found at richiejp.github.io/jdp or rpalethorpe.io.suse.de/jdpYou can also find documentation at the Julia REPL by typing ? followed by an identifier or in a notebook you can type @doc identifier in a code cell.The following image may give you some intuition for what JDP is.(Image: Outer Architecture)"
 },
 
 {
