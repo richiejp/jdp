02d3ae3c211669f25db678561f02e203ad2bfbce
Richard Palethorpe <rpalethorpe@suse.com>
2019-07-31 11:07:30 +0200

    Make OpenQA data refresh more robust and report accurate errors
    A failure on one tracker no longer prevents the refresh on other trackers from
    completing. Also an accurate stack trace is reported in the log and via a
    notification. Previously the stack trace was just from asyncmap's code which
    rethrows the child task's exception with `throw` instead of `rethrow`.

--- run/refresh-openqa.jl
+++ run/refresh-openqa.jl
@@ -6,26 +6,45 @@ using JDP.IOHelpers
 using JDP.Tracker
 using JDP.Trackers.OpenQA
 using JDP.Repository
+using JDP.Spammer
 
 argdefs = IOHelpers.ShellArgDefs(Set(["skipjobs"]), Dict())
 args = IOHelpers.parse_args(argdefs, ARGS).named
 
 trackers = [Tracker.get_tracker("osd"), Tracker.get_tracker("ooo")]
+errors = []
 
 asyncmap(trackers) do tracker
-    jobgroups = filter(Repository.refresh(tracker, OpenQA.JobGroup)) do g
-        if (toml = OpenQA.extract_toml(g.description)) == nothing || !haskey(toml, "JDP")
-            false
-        else
-            @info "$(tracker.tla): Found JDP config on Job Group $(g.name) ($(g.id))"
-            true
+    try
+        jobgroups = filter(Repository.refresh(tracker, OpenQA.JobGroup)) do g
+            if (toml = OpenQA.extract_toml(g.description)) == nothing || !haskey(toml, "JDP")
+                false
+            else
+                @info "$(tracker.tla): Found JDP config on Job Group $(g.name) ($(g.id))"
+                true
+            end
         end
-    end
 
-    if !args["skipjobs"]
-        Repository.refresh(tracker, jobgroups)
-        Repository.refresh(OpenQA.RecentOrInterestingJobsDef, tracker.tla)
-    else
-        @warn "$(tracker.tla): Skipping refreshing the jobs"
+        if !args["skipjobs"]
+            Repository.refresh(tracker, jobgroups)
+            Repository.refresh(OpenQA.RecentOrInterestingJobsDef, tracker.tla)
+        else
+            @warn "$(tracker.tla): Skipping refreshing the jobs"
+        end
+    catch error
+        @error "$(tracker.tla): Stopping due to an exception (details at end)"
+        push!(errors, (tracker, error, catch_backtrace()))
     end
+end
+
+for (tracker, error, backtrace) in errors
+    io = IOBuffer()
+    showerror(io, error, backtrace)
+    exception = String(take!(io))
+    @error "While processing $(tracker.tla): $exception"
+
+    msg = """
+**Data refresh failed for $(tracker.tla)!** :fearful:
+$exception"""
+    Spammer.post_message(msg, :rpalethorpe)
 end
